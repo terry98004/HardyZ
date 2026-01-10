eeaaeefec96391a5f29b3cb50879e06c59c3a469
Terry Murphy <vendor@tpmassoc.com>
2026-01-10 12:39:01 -0800

    Update makehardyz.bat
    Changed various flags

--- makehardyz.bat
+++ makehardyz.bat
@@ -1,3 +1,3 @@
-gcc -Wall -Wextra -pedantic-errors -std=c99 -posix -c hardyz.c 
-gcc -Wall -Wextra -pedantic-errors -std=c99 -posix -c comphardyz.c 
-gcc --static -o hardyz hardyz.o comphardyz.o  -L. -l:libhgt.a -l:libquadmath.a -l:libmpfr.a -l:libgmp.a
+gcc -Wall -Wextra -pedantic-errors -std=gnu17 -pthread -c hardyz.c 
+gcc -Wall -Wextra -pedantic-errors -std=gnu17 -pthread -c comphardyz.c 
+gcc -static -pthread -o hardyz hardyz.o comphardyz.o  -L. -l:libhgt.a -l:libmpfr.a -l:libgmp.a

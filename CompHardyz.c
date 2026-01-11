82ba950db206828114f8519cd3a60fed126c173e
Terry Murphy <vendor@tpmassoc.com>
2026-01-11 08:01:33 -0800

    Remove <time.h> include
    Remove <time.h> include, which is not used here.

--- CompHardyz.c
+++ CompHardyz.c
@@ -4,7 +4,6 @@
 // MIT License -- see hardyz.c for details.
 // -------------------------------------------------------------------
 
-#include <time.h>
 #include <stdio.h>
 #include <math.h>			
 #include <stdlib.h>
